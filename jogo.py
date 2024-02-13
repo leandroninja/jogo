16da6468947b177357a5d0549ff2fc90185ddf41
Leandro Oliveira Moraes <riey21@hotmail.com>
2024-02-13 19:13:06 -0300

    adicionado nosvos carros e nova logica na movimentação

--- jogo.py
+++ jogo.py
@@ -5,14 +5,18 @@ from random import randint
 pygame.init()
 
 # e a posição onde o carro vai aparecer
-#o pos_y controla autura onde o carro vai ficar
+#o pos_y controla a altura onde o carro vai ficar
 #o pos_x 
-x = 370        #valor max a esquerda 308, max dir 445,meio 370
-y = 200
+x = 360       #valor max a esquerda 308, max dir 445,meio 370
+y = 100
 pos_x = 300
 pos_y = 600
 pos_y_a = 600
+pos_y_b = 600
 pos_y_c = 600
+pos_y_d = 600
+pos_y_e = 600
+pos_y_f = 600
 timer = 0
 tempo_segundo = 0
 
@@ -20,10 +24,13 @@ velocidade = 10
 
 velocidade_outros = 5
 fundo = pygame.image.load("pista.png")
-carro = pygame.image.load("carro5.png")
+carro = pygame.image.load("carro1.png")
+carro2 = pygame.image.load("carro2.png")
+carro3 = pygame.image.load("carro3.png")
+carro4 = pygame.image.load("carro4.png")
+carro5 = pygame.image.load("carro5.png")
 jipe = pygame.image.load("jipe.png")
 jipe1 = pygame.image.load("jipe1.png")
-carro4 = pygame.image.load("carro4.png")
 
 font = pygame.font.SysFont ('arial black',30)
 texto = font.render("tempo: ",True,(255,255,255),(0,0,0))
@@ -34,7 +41,7 @@ pos_texto.center = (70,20)
 # se não iniciar use o comando python -m pip install pygame
 
 # definindo tamanho da janela
-janela = pygame.display.set_mode ((800,500))
+janela = pygame.display.set_mode ((800,619))
 
 #vamos usar o metodo a baixo e onde vai fica o nome na janela que vai aparecer
 pygame.display.set_caption("Criando um jogo com python")
@@ -47,23 +54,27 @@ while janela_aberta :
     pygame.time.delay(50)
     #quando for disparado algum evento ele vai cair dentro desse for
     for event in pygame.event.get():
-        if event.type == pygame.quit:
+        if event.type == pygame.QUIT:
             janela_aberta = False
 
     comandos = pygame.key.get_pressed()
- #aumenta ou diminui a posição na tela if comandos[pygame.K_UP]:
-  #          y -=velocidade  
-  #  if comandos[pygame.K_DOWN]:
-  #          y +=velocidade
-    if comandos[pygame.K_LEFT] and x >= 308:
-            x -= velocidade
-    if comandos[pygame.K_RIGHT] and x <= 445:
+    if comandos[pygame.K_UP]:
+            y -=velocidade  
+    if comandos[pygame.K_DOWN]:
+            y +=velocidade
+    if comandos[pygame.K_LEFT] and x>= 125:       #308
+            x -= velocidade                        #na pista propria
+    if comandos[pygame.K_RIGHT] and x<= 620:      #445
             x += velocidade   
 #com esse if a baixo resetamos a posição dos carros apos passarem pela pista
-    if  (pos_y <= -180) and (pos_y_a <= -180) and (pos_y_c <= -180):
-         pos_y = randint (800, 2000)
-         pos_y_a = randint (800, 2000)
-         pos_y_c = randint (800, 2000) 
+    if  (pos_y <= -180) and (pos_y_a <= -180) and (pos_y_b <= -180) and (pos_y_c <= -180) and (pos_y_d <= -180) and (pos_y_e <= -180):
+         pos_y = randint (1000, 1500)
+         pos_y_a = randint (1000, 1400)
+         pos_y_b = randint (1000, 1300)
+         pos_y_c = randint (1000, 1200) 
+         pos_y_d = randint (1000, 1100)
+         pos_y_e = randint (900, 1000)
+         pos_y_f = randint (800, 900)
 
 #logica do if else todo esse bloco de codigo esta dentro do while esse while tem delay que atualiza a tela a cada 50 mls
  #mas a ideia e que a variavel seja incrementada a cada 1 seg ou seja vai passar pelo while 20 vezes a cada 50 mils       
@@ -79,13 +90,27 @@ while janela_aberta :
 #a função a baixo muda a posição dos carros no eixo y
     pos_y  -= velocidade_outros
     pos_y_a -= velocidade_outros -2
-    pos_y_c -= velocidade_outros - 1   
+    pos_y_b -= velocidade_outros - 1 
+    pos_y_c -= velocidade_outros - 2
+    pos_y_d -= velocidade_outros - 1
+    pos_y_e -= velocidade_outros - 2 
+    pos_y_f -= velocidade_outros - 1    
+
+
+
+
 
     janela.blit(fundo,(0,0))  
     janela.blit(carro,(x,y))
+    janela.blit(carro2, (pos_x + 60, pos_y_b))
+    janela.blit(carro3, (pos_x + 120, pos_y_c))
+    janela.blit(carro4, (pos_x + 240, pos_y_d))
+    janela.blit(carro5, (pos_x + -50, pos_y_f))
     janela.blit(jipe, (pos_x + 100, pos_y))
-    janela.blit(jipe1, (pos_x + 80, pos_y_a))
-    janela.blit(carro4, (pos_x + 70, pos_y_c))
+    janela.blit(jipe1, (pos_x - 90 , pos_y_a))
+    
+
+
     janela.blit(texto,pos_texto)
     pygame.display.update()
 
